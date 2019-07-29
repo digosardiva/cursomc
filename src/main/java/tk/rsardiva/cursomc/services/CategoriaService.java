94d8b9e31a2b3cb16f96744f37e14dea77616d88
Rodrigo Sardiva <digosardiva@hotmail.com>
2019-07-29 15:13:58 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/tk/rsardiva/cursomc/services/CategoriaService.java
+++ src/main/java/tk/rsardiva/cursomc/services/CategoriaService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import tk.rsardiva.cursomc.domain.Categoria;
 import tk.rsardiva.cursomc.repositories.CategoriaRepository;
+import tk.rsardiva.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> categoria = repo.findById(id);
-		return categoria.orElse(null);		
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));	
 	}
 	
 }
