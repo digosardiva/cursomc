1046cb8904c4977fc021bc7e1ded6f832f27242b
Rodrigo Sardiva <digosardiva@hotmail.com>
2019-08-05 11:48:06 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/tk/rsardiva/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package tk.rsardiva.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+import tk.rsardiva.cursomc.domain.Cliente;
+import tk.rsardiva.cursomc.repositories.ClienteRepository;
+import tk.rsardiva.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> categoria = repo.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));	
+	}
+	
+}
