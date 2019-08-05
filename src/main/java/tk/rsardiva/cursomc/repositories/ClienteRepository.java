581602789e05084ddfec377bbc0cc2d17430a99d
Rodrigo Sardiva <digosardiva@hotmail.com>
2019-08-05 11:27:38 -0300

    Cliente, Tipo Cliente, telefones e enderecos

---
+++ src/main/java/tk/rsardiva/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package tk.rsardiva.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import tk.rsardiva.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
